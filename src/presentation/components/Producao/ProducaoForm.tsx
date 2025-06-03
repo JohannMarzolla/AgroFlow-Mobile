ed35eca018acd24f8f0afc0b74237f3712c24071
Johann Marzolla <email@fiap.com.br>
2025-06-03 16:44:32 -0300

    Alterando formularios para useForm + zod

--- src/presentation/components/Producao/ProducaoForm.tsx
+++ src/presentation/components/Producao/ProducaoForm.tsx
@@ -1,96 +1,149 @@
-import { Produto } from "@/domain/models/Produto";
 import { useProducao } from "@/presentation/contexts/ProducaoContext";
 import { useProdutos } from "@/presentation/contexts/ProdutoContext";
 import { Picker } from "@react-native-picker/picker";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import React, { useState } from "react";
-import { View, Text, TextInput, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import { Controller, useForm } from "react-hook-form";
 import { ShowToast } from "../ui/Toast";
 
+
+const producaoSchema = z.object({
+  produto: z.object({
+    id: z.string(),
+    nome: z.string(),
+    unidadeMedida: z.string(),
+  }),
+  quantidade: z.coerce.number().positive("A quantidade deve ser positiva"),
+  status: z.string().min(1, "Status é obrigatório"),
+});
+
+type ProducaoFormData = z.infer<typeof producaoSchema>;
+
 export default function ProducaoForm() {
-  const [quantidade, setQuantidade] = useState<number>(0);
-  const [status, setStatus] = useState<string>("");
-  const [produtoSelecionado, setProdutoSelecionado] = useState<Produto>();
-  const [data, setData] = useState<Date>();
   const [loading, setLoading] = useState(false);
-  const {adicionarProducao} = useProducao();
-  const {produtos} = useProdutos();
+  const { adicionarProducao } = useProducao();
+  const { produtos } = useProdutos();
 
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<ProducaoFormData>({
+    resolver: zodResolver(producaoSchema),
+    defaultValues: {
+      quantidade: 0,
+      status: "",
+    },
+  });
 
-
-  const limparCampo = () =>{
-    setQuantidade(0),
-    setStatus("")
-    // setProdutoSelecionado(),
-
-  }
-
-  const handleSubmit = async () => {
-    if (!produtoSelecionado) {
-      ShowToast("error", "Você deve selecionar um produto.");
-      return;
-      }
-  try {
-    setLoading(true);
-     console.log("produto selecionado ", produtoSelecionado)
-    
-     await adicionarProducao({
-        quantidade: quantidade,
-        status: status,
+  const onSubmit = async (data: ProducaoFormData) => {
+    try {
+      setLoading(true);
+      await adicionarProducao({
+        quantidade: data.quantidade,
+        status: data.status,
         data: new Date(),
-        produto: {
-          id: produtoSelecionado.id,
-          nome: produtoSelecionado.nome,
-          unidadeMedida: produtoSelecionado.unidadeMedida
-        }
+        produto: data.produto,
       });
-    limparCampo();
-  } catch (error) {
-    console.error("Erro ao adicionar produto", error);
-  } finally {
-    setLoading(false);
-  }
-};
+      reset(); 
+      ShowToast("success", "Produção adicionada com sucesso!");
+    } catch (error) {
+      console.error("Erro ao adicionar produto", error);
+      ShowToast("error", "Erro ao adicionar produção");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <View>
-      <Text className="text-xl font-semibold mb-4">Nome do Produto</Text>
-          <Picker
-        selectedValue={produtoSelecionado}
-        onValueChange={(itemValue) => setProdutoSelecionado(itemValue)}
-        className="border border-gray-300 rounded mb-4"
-      >
-        <Picker.Item label="Selecione um produto" value="" />
-        {produtos.map((produto) => (
-          <Picker.Item
-            key={produto.id}
-            label={produto.nome}
-            value={produto}
-           
-          />
-        ))}
-      </Picker>
-      <Text className="text-xl font-semibold mb-4">Quantidade</Text>
-     <TextInput
-      className="border border-gray-300 rounded px-3 py-2 mb-4"
-      placeholder="Ex: 500"
-      value={quantidade.toString()}
-      keyboardType="numeric"
-      onChangeText={(text) => setQuantidade(Number(text))}
-    />
+      <View >
+        <Text className="text-xl font-semibold mb-2">Nome do Produto</Text>
+        <Controller
+          control={control}
+          name="produto"
+          render={({ field: { onChange, value } }) => (
+            <Picker
+              selectedValue={value}
+              onValueChange={(itemValue) => onChange(itemValue)}
+              enabled={!loading}
+              className="border border-gray-300 rounded"
+            >
+              <Picker.Item label="Selecione um produto" value={undefined} />
+              {produtos.map((produto) => (
+                <Picker.Item
+                  key={produto.id}
+                  label={produto.nome}
+                  value={produto}
+                />
+              ))}
+            </Picker>
+          )}
+        />
+        {errors.produto && (
+          <Text className="text-red-500 mt-1">{errors.produto.message}</Text>
+        )}
+     
 
-      <Text className="text-xl font-semibold mb-4">Status de Produção</Text>
-      <TextInput
-        className="border border-gray-300 rounded px-3 py-2 mb-4"
-        placeholder="Ex: em produção"
-        value={status}
-        onChangeText={setStatus}
-      />
+      <View className="mb-4">
+        <Text className="text-xl font-semibold mb-2">Quantidade</Text>
+        <Controller
+          control={control}
+          name="quantidade"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              className="border border-gray-300 rounded px-3 py-2"
+              placeholder="Ex: 500"
+              keyboardType="numeric"
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value?.toString()}
+              editable={!loading}
+            />
+          )}
+        />
+        {errors.quantidade && (
+          <Text className="text-red-500 mt-1">{errors.quantidade.message}</Text>
+        )}
+      </View>
+
+      <View className="mb-4">
+        <Text className="text-xl font-semibold mb-2">Status de Produção</Text>
+        <Controller
+          control={control}
+          name="status"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              className="border border-gray-300 rounded px-3 py-2"
+              placeholder="Ex: Em produção"
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+              editable={!loading}
+            />
+          )}
+        />
+        {errors.status && (
+          <Text className="text-red-500 mt-1">{errors.status.message}</Text>
+        )}
+      </View>
 
       <Pressable
-        className="bg-green-600 p-3 rounded"
-        onPress={handleSubmit}
+        className="bg-green-600 p-3 rounded mt-2"
+        onPress={handleSubmit(onSubmit)}
+        disabled={loading}
       >
-        <Text className="text-white text-center font-semibold">Salvar</Text>
+        <Text className="text-white text-center font-semibold">
+          {loading ? "Salvando..." : "Salvar"}
+        </Text>
       </Pressable>
     </View>
   );
--- src/presentation/components/Produto/ProdutoForm.tsx
+++ src/presentation/components/Produto/ProdutoForm.tsx
@@ -1,54 +1,108 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, Controller } from "react-hook-form";
+import { View, Text, TextInput, Pressable, ActivityIndicator} from "react-native";
 import { useProdutos } from "@/presentation/contexts/ProdutoContext";
+import { ShowToast } from "../ui/Toast";
 import React, { useState } from "react";
-import { View, Text, TextInput, Pressable } from "react-native";
+
+const produtoSchema = z.object({
+  nome: z.string().min(1, "Nome é obrigatório"),
+  unidadeMedida: z.string().min(1, "Unidade de medida é obrigatória"),
+});
+
+type ProdutoFormData = z.infer<typeof produtoSchema>;
 
 export default function ProdutoForm() {
-  const [nome, setNome] = useState<string>("");
-  const [unidadeMedida, setUnidadeMedida] = useState<string>("");
-  const {adicionarProduto} = useProdutos();
-  const [loading, setLoading] = useState(false)
+  const { adicionarProduto } = useProdutos();
+  const [loading, setLoading] = useState(false);
 
-  const limparCampo = () =>{
-    setNome(""),
-    setUnidadeMedida("")
-  }
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ProdutoFormData>({
+    resolver: zodResolver(produtoSchema),
+    defaultValues: {
+      nome: "",
+      unidadeMedida: ""
+    }
+  });
 
-  const handleSubmit = async () => {
-  try {
-    setLoading(true);
-    await adicionarProduto({nome: nome.trim(), unidadeMedida: unidadeMedida.trim()});
-    limparCampo();
-  } catch (error) {
-    console.error("Erro ao adicionar produto", error);
-  } finally {
-    setLoading(false);
-  }
-};
+  const onSubmit = async (data: ProdutoFormData) => {
+    try {
+      setLoading(true);
+      await adicionarProduto(data);
+      ShowToast("success", "Produto cadastrado com sucesso!");
+      reset();
+    } catch (error) {
+      console.error("Erro ao adicionar produto", error);
+      ShowToast("error", "Erro ao salvar o produto.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <View>
-      <Text className="text-xl font-semibold mb-4">Nome do Produto</Text>
-      <TextInput
-        className="border border-gray-300 rounded px-3 py-2 mb-4"
-        placeholder="Ex: Arroz"
-        value={nome}
-        onChangeText={setNome}
-        
-      />
-
-      <Text className="text-xl font-semibold mb-4">Unidade de Medida</Text>
-      <TextInput
-        className="border border-gray-300 rounded px-3 py-2 mb-4"
-        placeholder="Ex: kg, unidade, pacote"
-        value={unidadeMedida}
-        onChangeText={setUnidadeMedida}
-      />
+    
+      <View >
+        <Text className="text-xl font-semibold mb-2 ">Nome do Produto</Text>
+        <Controller
+          control={control}
+          name="nome"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              className="border border-gray-300 rounded px-3 py-2 mb-1 "
+              placeholder="Ex: Arroz"
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              editable={!loading}
+            />
+          )}
+        />
+        {errors.nome && (
+          <View className="flex flex-row items-center mt-1">
+            <Text className="text-red-500 ml-1 text-x">{errors.nome.message}</Text>
+          </View>
+        )}
+      
+      <View className="mb-6">
+        <Text className="text-xl font-semibold mb-2">Unidade de Medida</Text>
+        <Controller
+          control={control}
+          name="unidadeMedida"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              className="border border-gray-300 rounded px-3 py-2 mb-1 "
+              placeholder="Ex: kg, un, pacote"
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              editable={!loading}
+            />
+          )}
+        />
+        {errors.unidadeMedida && (
+          <View className="flex flex-row items-center mt-1">
+            <Text className="text-red-500 ml-1 text-x">{errors.unidadeMedida.message}</Text>
+          </View>
+        )}
+      </View>
 
       <Pressable
-        className="bg-green-600 p-3 rounded"
-        onPress={handleSubmit}
+        className={`p-4 rounded-lg flex-row justify-center items-center ${loading ? "bg-gray-400" : "bg-green-600"}`}
+        onPress={handleSubmit(onSubmit)}
+        disabled={loading}
       >
-        <Text className="text-white text-center font-semibold">Salvar</Text>
+        {loading ? (
+          <ActivityIndicator color="white" />
+        ) : (
+          <>
+            <Text className="text-white font-medium">Salvar</Text>
+          </>
+        )}
       </Pressable>
     </View>
   );
